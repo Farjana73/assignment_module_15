d2b392e80cc022ed01bef55453f28924173fa4dc
Farjana73 <faakther73@gmail.com>
2024-08-25 22:31:17 +0600

    update

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-// //* */  question 01 and 02 server create & route handling
-//
-//
-// const http = require("http");
-//
-// const server = http.createServer(function(req,res){
-//
-//     if(req.url=="/"){
-//         res.end("This is the Home Page")
-//     }
-//     if(req.url=="/about"){
-//         res.end("This is the About Page")
-//     }
-//     if(req.url=="/contact"){
-//         res.end("This is the Contact Page")
-//     }
-//
-//
-// })
-//
-//
-// server.listen(5500, function(){
-//     console.log("server running on 5500")
-// })
 
 const http = require('http');
 const fs = require('fs')
